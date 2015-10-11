f48bf964d35751518fd6524da8487efd9b8ab63a
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2015-10-11 22:36:41 +0200

    Add lib and example
    Signed-off-by: Drasko DRASKOVIC <drasko.draskovic@gmail.com>

--- examples/mfDevice.js
+++ examples/mfDevice.js
@@ -9,5 +9,86 @@
 var mf   = require ('../');
 var mfClient = mf.client();
 
-mfClient.connect();
-mfClient.getStatus();
+var DEVICE_TOKEN = "MY_SECRET_TOKEN"
+
+var myTestDeviceId = "";
+
+
+/**
+ * setToken();
+ */
+mfClient.setToken(DEVICE_TOKEN);
+
+
+/**
+ * getStatus()
+ */
+mfClient.getStatus(function (err, rsp, body) {
+    if (!err && rsp.statusCode == 200) {
+        console.log(body);
+    }
+});
+
+
+/**
+ * getDevices()
+ */
+mfClient.getDevices(function (err, rsp, body) {
+    if (!err && rsp.statusCode == 200) {
+        console.log(body);
+    }
+});
+
+
+/**
+ * createDevice()
+ */
+mfClient.createDevice(function (err, rsp, body) {
+    if (!err && rsp.statusCode == 200) {
+        console.log(body);
+
+        myTestDeviceId = JSON.parse(body)._id;
+        console.log("myTestDevice = ", myTestDeviceId);
+    }
+});
+
+
+/**
+ * getDevice()
+ */
+mfClient.getDevice(myTestDeviceId, function (err, rsp, body) {
+    if (!err && rsp.statusCode == 200) {
+        console.log(body);
+    }
+});
+
+
+/**
+ * deleteDevice()
+ */
+mfClient.getDevice(myTestDeviceId, function (err, rsp, body) {
+    if (!err && rsp.statusCode == 200) {
+        console.log(body);
+    }
+});
+
+
+/**
+ * getDevice() - try to fetch it again (should be empty JSON now)
+ */
+mfClient.getDevice(myTestDeviceId, function (err, rsp, body) {
+    if (!err && rsp.statusCode == 200) {
+        console.log(body);
+    }
+});
+
+
+/**
+ * getDevices()
+ */
+mfClient.getDevices(function (err, rsp, body) {
+    if (!err && rsp.statusCode == 200) {
+        console.log(body);
+    }
+});
+
